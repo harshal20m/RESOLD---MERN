1ea543171dd723ca8438417824c2691d3c7eece1
harshal mali <20harshalmali@gmail.com>
2024-10-08 18:14:02 +0530

    url updates

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
 	// baseURL: "http://localhost:5000/", // Your backend base URL
-	baseURL: "http://3.88.133.68:5000/",
+	baseURL: "https://resold-mern.onrender.com",
 });
 
 export default axiosInstance;
