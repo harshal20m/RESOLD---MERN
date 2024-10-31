8f2b8315f4d464e6cc2bac00522d89e9da59493c
harshal mali <20harshalmali@gmail.com>
2024-10-31 22:36:08 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,9 +12,7 @@ import SearchResults from "./components/SearchResults";
 import ErrorBoundary from "./components/ErrorBoundary";
 import SignIn from "./components/SignIn";
 import AboutUs from "./components/AboutUs";
-import Loading from "./components/Loading";
 import { useEffect, useState } from "react";
-import axiosInstance from "./api";
 
 const ProtectedRoute = ({ children }) => {
 	const token = localStorage.getItem("token");
@@ -22,39 +20,58 @@ const ProtectedRoute = ({ children }) => {
 };
 
 function App() {
-	const [loading, setLoading] = useState(false);
+	const [loading, setLoading] = useState(true);
+	const [timeLeft, setTimeLeft] = useState(90); // Countdown timer
 
 	useEffect(() => {
-		//request interceptor
-
-		axiosInstance.interceptors.request.use(
-			(config) => {
-				setLoading(true);
-				return config;
-			},
-			(error) => {
-				return Promise.reject(error);
-			}
-		);
-		//response interceptor
-		axiosInstance.interceptors.response.use(
-			(config) => {
+		const fetchData = async () => {
+			const response = await fetch("https://resold-mern.onrender.com/allproducts");
+			if (response.ok) {
+				// Handle your data here, e.g., set state
 				setLoading(false);
-				return config;
-			},
-			(error) => {
-				return Promise.reject(error);
+			} else {
+				// Handle error
+				setLoading(false);
 			}
-		);
+		};
+
+		fetchData();
 	}, []);
+
+	useEffect(() => {
+		if (loading) {
+			const timer = setInterval(() => {
+				setTimeLeft((prev) => {
+					if (prev <= 1) {
+						clearInterval(timer);
+						window.location.reload(); // Refresh the page
+						return 0; // Stop countdown
+					}
+					return prev - 1;
+				});
+			}, 1000);
+
+			return () => clearInterval(timer); // Clean up on unmount
+		}
+	}, [loading]);
 
 	return (
 		<ErrorBoundary>
 			<Router>
 				<div className="flex flex-col min-h-screen h-full w-full">
 					<Navbar />
-					<Loading show={loading} />
-
+					{loading && (
+						<div className="loading-container">
+							<p className="loading-text">
+								Thanks for visiting. <br />
+								It's a full-stack project, so we are running our backend on a public cloud.
+								<br />
+								Loading in {timeLeft} seconds or maybe less. <br />
+								Thanks for your patience. Our website instance will run soon. <br />
+								Website will refresh after timeout.
+							</p>
+						</div>
+					)}
 					<main className="flex-grow">
 						<Routes>
 							<Route path="/" element={<Home />} />
@@ -80,8 +97,6 @@ function App() {
 							/>
 							<Route path="/items/:id" element={<ItemDetails />} />
 							<Route path="/search" element={<SearchResults />} />
-
-							{/* footer links here */}
 							<Route path="/about" element={<AboutUs />} />
 						</Routes>
 					</main>
--- frontend/src/components/Items.jsx
+++ frontend/src/components/Items.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import axiosInstance from "../api";
 import Pagination from "./Pagination";
+import Loading from "./Loading"; // Import your loading component
 
 function Items() {
 	const [items, setItems] = useState([]);
@@ -9,9 +10,11 @@ function Items() {
 	const [totalPages, setTotalPages] = useState(1);
 	const [isModalOpen, setIsModalOpen] = useState(false);
 	const [selectedItemId, setSelectedItemId] = useState(null);
+	const [loading, setLoading] = useState(true); // Add loading state
 	const navigate = useNavigate();
 
 	const fetchItems = async (page) => {
+		setLoading(true); // Set loading to true before fetching
 		try {
 			const response = await axiosInstance.get("/items", {
 				params: { page, limit: 8 },
@@ -21,6 +24,8 @@ function Items() {
 			setTotalPages(response.data.totalPages);
 		} catch (error) {
 			console.error("Error fetching items:", error);
+		} finally {
+			setLoading(false); // Set loading to false after fetching
 		}
 	};
 
@@ -53,109 +58,116 @@ function Items() {
 			<div className="container mx-auto p-4 flex flex-col">
 				<h1 className="text-2xl font-bold mb-4 text-center text-white">Items for Sale</h1>
 
-				{/* Responsive Grid Layout */}
-				<div className="grid mx-auto grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
-					{items.map((item) => (
-						<div
-							key={item._id}
-							className="max-w-full sm:max-w-xs bg-gradient-180 w-full rounded-lg shadow mb-5 bg-black flex flex-col justify-between"
-						>
-							<Link to={`/items/${item._id}`}>
-								{item.images && item.images.length > 0 && (
-									<img
-										className="rounded-t-lg w-full h-48 object-cover"
-										src={item.images[0].url} // Use the Cloudinary image URL
-										alt={item.title}
-									/>
-								)}
-								<div className="p-4 flex flex-col">
-									<h5 className="mb-2 text-lg md:text-2xl font-bold tracking-tight text-white">
-										{item.title}
-									</h5>
+				{/* Show Loading Component */}
+				{loading ? (
+					<Loading />
+				) : (
+					// Responsive Grid Layout
+					<div className="grid mx-auto grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
+						{items.map((item) => (
+							<div
+								key={item._id}
+								className="max-w-full sm:max-w-xs bg-gradient-180 w-full rounded-lg shadow mb-5 bg-black flex flex-col justify-between"
+							>
+								<Link to={`/items/${item._id}`}>
+									{item.images && item.images.length > 0 && (
+										<img
+											className="rounded-t-lg w-full h-48 object-cover"
+											src={item.images[0].url} // Use the Cloudinary image URL
+											alt={item.title}
+										/>
+									)}
+									<div className="p-4 flex flex-col">
+										<h5 className="mb-2 text-lg md:text-2xl font-bold tracking-tight text-white">
+											{item.title}
+										</h5>
 
-									<p className="mb-3 font-normal text-gray-300 text-sm md:text-base">
-										{item.description}
-									</p>
-									<p className="text-white mb-4 text-sm md:text-base">Price: ₹ {item.price}/-</p>
-									<p className="text-white mb-4 text-sm md:text-base">
-										Seller: {item.user ? item.user.username : "Unknown"}
-									</p>
-									<div className="flex justify-between items-center">
-										<div className="inline-flex items-center px-3 py-2 text-xs md:text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-											View Details
-											<svg
-												className="w-3.5 h-3.5 ms-2"
-												aria-hidden="true"
-												xmlns="http://www.w3.org/2000/svg"
-												fill="none"
-												viewBox="0 0 14 10"
-											>
-												<path
-													stroke="currentColor"
-													strokeLinecap="round"
-													strokeLinejoin="round"
-													strokeWidth="2"
-													d="M1 5h12m0 0L9 1m4 4L9 9"
-												/>
-											</svg>
-										</div>
-										{item.user && item.user._id === userId && (
-											<div className="flex space-x-1">
-												<button
-													onClick={() => handleEdit(item._id)}
-													className="bg-yellow-500 text-white px-3 py-1 rounded-full text-xs"
+										<p className="mb-3 font-normal text-gray-300 text-sm md:text-base">
+											{item.description}
+										</p>
+										<p className="text-white mb-4 text-sm md:text-base">Price: ₹ {item.price}/-</p>
+										<p className="text-white mb-4 text-sm md:text-base">
+											Seller: {item.user ? item.user.username : "Unknown"}
+										</p>
+										<div className="flex justify-between items-center">
+											<div className="inline-flex items-center px-3 py-2 text-xs md:text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+												View Details
+												<svg
+													className="w-3.5 h-3.5 ms-2"
+													aria-hidden="true"
+													xmlns="http://www.w3.org/2000/svg"
+													fill="none"
+													viewBox="0 0 14 10"
 												>
-													Edit
-												</button>
-												<button
-													className="bg-red-500 px-3 py-2 rounded-full text-xs text-white"
-													onClick={() => {
-														setSelectedItemId(item._id);
-														setIsModalOpen(true);
-													}}
-												>
-													Delete
-												</button>
+													<path
+														stroke="currentColor"
+														strokeLinecap="round"
+														strokeLinejoin="round"
+														strokeWidth="2"
+														d="M1 5h12m0 0L9 1m4 4L9 9"
+													/>
+												</svg>
 											</div>
-										)}
+											{item.user && item.user._id === userId && (
+												<div className="flex space-x-1">
+													<button
+														onClick={() => handleEdit(item._id)}
+														className="bg-yellow-500 text-white px-3 py-1 rounded-full text-xs"
+													>
+														Edit
+													</button>
+													<button
+														className="bg-red-500 px-3 py-2 rounded-full text-xs text-white"
+														onClick={() => {
+															setSelectedItemId(item._id);
+															setIsModalOpen(true);
+														}}
+													>
+														Delete
+													</button>
+												</div>
+											)}
+										</div>
 									</div>
-								</div>
-							</Link>
-						</div>
-					))}
-				</div>
-			</div>
-
-			{totalPages > 1 && (
-				<Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} />
-			)}
-
-			{/* Delete Confirmation Modal */}
-			{isModalOpen && (
-				<dialog open className="modal modal-bottom sm:modal-middle">
-					<div className="modal-box bg-red-500 text-white">
-						<h3 className="font-bold text-lg">Delete</h3>
-						<p className="py-4">Are you sure you want to delete this item? Changes cannot be reverted!</p>
-						<div className="modal-action">
-							<button
-								className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-								onClick={() => setIsModalOpen(false)}
-							>
-								✕
-							</button>
-							<button
-								className="btn hover:bg-red-800 hover:text-white"
-								onClick={() => {
-									handleDelete(selectedItemId);
-									setIsModalOpen(false);
-								}}
-							>
-								Delete
-							</button>
-						</div>
+								</Link>
+							</div>
+						))}
 					</div>
-				</dialog>
-			)}
+				)}
+
+				{totalPages > 1 && (
+					<Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} />
+				)}
+
+				{/* Delete Confirmation Modal */}
+				{isModalOpen && (
+					<dialog open className="modal modal-bottom sm:modal-middle">
+						<div className="modal-box bg-red-500 text-white">
+							<h3 className="font-bold text-lg">Delete</h3>
+							<p className="py-4">
+								Are you sure you want to delete this item? Changes cannot be reverted!
+							</p>
+							<div className="modal-action">
+								<button
+									className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+									onClick={() => setIsModalOpen(false)}
+								>
+									✕
+								</button>
+								<button
+									className="btn hover:bg-red-800 hover:text-white"
+									onClick={() => {
+										handleDelete(selectedItemId);
+										setIsModalOpen(false);
+									}}
+								>
+									Delete
+								</button>
+							</div>
+						</div>
+					</dialog>
+				)}
+			</div>
 		</div>
 	);
 }
